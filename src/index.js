ec8421946ded569dc94c7712f9a9ed8b5b1cdff9
Cody Swartz <ctsstc@gmail.com>
2019-04-10 11:10:33 -0700

    Adds button with alert on click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("We're LIVE!!!");
+import elementReady from 'element-ready';
+
+import button from './ui/button';
+
+elementReady(".p-customize_emoji_wrapper").then(element => {
+  const injectBefore = '.p-customize_emoji_wrapper';
+  const beforeElement = document.querySelector(injectBefore);
+  
+  button.onclick = () => alert("We're LIVE!!!");  
+  beforeElement.before(button);
+});
---
+++ src/ui/button.js
@@ -0,0 +1,6 @@
+import htm from "htm";
+import h from "hyperscript";
+
+const html = htm.bind(h);
+
+export default html`<button>Hello World</button>`;
