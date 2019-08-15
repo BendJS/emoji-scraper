592fa324e1026604e98e090089fd4191e786b8ac
Cody Swartz <ctsstc@gmail.com>
2019-08-15 03:59:58 -0700

    Insert a button and alert We're back to where we started lol

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import elementReady from 'element-ready';
-
 import button from './ui/button';
 
-elementReady(".p-customize_emoji_wrapper").then(element => {
-  const injectBefore = '.p-customize_emoji_wrapper';
-  const beforeElement = document.querySelector(injectBefore);
-  
-  button.onclick = () => alert("We're LIVE!!!");  
-  beforeElement.before(button);
-});
+(async () => {
+  const selector = '#list_emoji_section';
+  const element = await elementReady(selector);
+  button.onclick = () => alert("We're LIVE!!!");
+  element.insertBefore(button, element.childNodes[0]);
+})();
